8887e9f397d65152d0d4cce2f43adcc4319ef607
abs <abann.sunny@tcengine.com>
2024-11-26 18:53:25 -0800

    add console app to the docker compose

---
+++ app/backend/TestApi.Domain/Models/ProfitRow.cs
@@ -0,0 +1,8 @@
+namespace TestApi.Domain.Models;
+
+public class ProfitRow
+{
+    public string Quarter { get; set; }
+    public string Year { get; set; }
+    public string Profit { get; set; }
+}

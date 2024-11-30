e0bd0deab2e387e969c7d9b492abf4fc5a46d9e1
Choudhry Shehryar <shahriyarraheel786@gmail.com>
2024-11-30 23:37:14 +0500

    minor fixes: vllm-docker

--- deployment/docker/GPU/02-vllm-tensor/python/benchmark-offline-serving.py
+++ deployment/docker/GPU/02-vllm-tensor/python/benchmark-offline-serving.py
@@ -32,7 +32,7 @@ class LLMPredictor:
 
     def __init__(self):
         # Create an LLM.
-        self.llm = LLM(model="mistralai/Ministral-8B-Instruct-2410", tensor_parallel_size=tensor_parallel_size, max_num_batched_tokens=24000, max_model_len=512, max_num_seqs=40, dtype="half", tokenizer-mode="mistral")
+        self.llm = LLM(model="mistralai/Ministral-8B-Instruct-2410", tensor_parallel_size=tensor_parallel_size, max_num_batched_tokens=24000, max_model_len=512, max_num_seqs=40, dtype="half", tokenizer_mode="mistral")
 
     def __call__(self, batch: Dict[str, np.ndarray]) -> Dict[str, list]:
         # Generate texts from the prompts.
@@ -94,7 +94,7 @@ ds = ds.map_batches(
 # Peek first 10 results.
 # NOTE: This is for local testing and debugging. For production use case,
 # one should write full result out as shown below.
-outputs = ds.take(limit=10)
+outputs = ds.take(limit=5800)
 for output in outputs:
     prompt = output["prompt"]
     generated_text = output["generated_text"]
